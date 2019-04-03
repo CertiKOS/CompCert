0962235d3638681a33b2db57834114c2cb26362c
Jeremie Koenig <jeremie.koenig@yale.edu>
2019-04-03 15:43:26 -0400

    Interaction monad: simplify abstraction

--- games/Interaction.v
+++ games/Interaction.v
@@ -941,39 +941,34 @@ Module Behavior.
   Qed.
 
   Section SIM.
-    Context `{Wkw: KripkeFrame unit}.
+    Context {W : Type}.
     Context {M1 M2} (RM : klr W M1 M2).
     Context {N1 N2} (RN : klr W N1 N2).
-    Context {A B} (R : klr W A B).
+    Context {A B} (R : rel A B).
 
-    Inductive pull_has w (x2 : t M2 N2 B) : trace M1 N1 A -> Prop :=
+    Inductive pull_has (x2 : t M2 N2 B) : trace M1 N1 A -> Prop :=
       | pull_val a b :
           has x2 (val b) ->
-          R w a b ->
-          pull_has w x2 (val a)
+          R a b ->
+          pull_has x2 (val a)
       | pull_div :
           has x2 div ->
-          pull_has w x2 div
+          pull_has x2 div
       | pull_undef t :
           has x2 undef ->
-          pull_has w x2 t
-      | pull_move w' m1 m2 :
+          pull_has x2 t
+      | pull_move w m1 m2 :
           has x2 (move m2) ->
-          w ~> w' ->
-          RM w' m1 m2 ->
-          pull_has w x2 (move m1)
-      | pull_tcons w' m1 m2 n1 t1 :
+          RM w m1 m2 ->
+          pull_has x2 (move m1)
+      | pull_tcons w m1 m2 n1 t1 :
           has x2 (move m2) ->
-          w ~> w' ->
-          RM w' m1 m2 ->
-          (forall w'' n2,
-              w' ~> w'' ->
-              RN w'' n1 n2 ->
-              pull_has w'' (delta x2 m2 n2) t1) ->
-          pull_has w x2 (tcons m1 n1 t1).
+          RM w m1 m2 ->
+          (forall n2, RN w n1 n2 -> pull_has (delta x2 m2 n2) t1) ->
+          pull_has x2 (tcons m1 n1 t1).
 
-    Lemma pull_has_undef_inv w y t :
-      pull_has w y undef -> has y t.
+    Lemma pull_has_undef_inv y t :
+      pull_has y undef -> has y t.
     Proof.
       inversion 1; eauto using closed.
     Qed.
@@ -981,34 +976,34 @@ Module Behavior.
     Hint Constructors pull_has.
     Hint Resolve pull_has_undef_inv.
 
-    Program Definition pull w (x2 : t M2 N2 B) :=
-      {| has := pull_has w x2 |}.
+    Program Definition pull (x2 : t M2 N2 B) :=
+      {| has := pull_has x2 |}.
     Next Obligation.
-      intros w x2 s t Ht Hst. revert s Hst.
+      intros x2 s t Ht Hst. revert s Hst.
       induction Ht; intros; eauto;
       inversion Hst; clear Hst; subst; eauto.
     Qed.
 
     Global Instance pull_ref :
-      Monotonic (@pull) (forallr - @ w, ref ++> ref).
+      Monotonic (@pull) (ref ++> ref).
     Proof.
-      intros w x y Hxy t Ht.
+      intros x y Hxy t Ht.
       revert y Hxy. cbn in *.
       induction Ht; intros; eauto.
       eapply pull_tcons; eauto.
-      intros w'' n2 Hw'' Hn.
-      edestruct H3; eauto.
+      intros n2 Hn.
+      edestruct H2; eauto.
       eapply delta_ref; eauto.
     Qed.
 
-    Lemma join_pull w x y:
-      ref (join (pull w x) (pull w y)) (pull w (join x y)).
+    Lemma join_pull x y:
+      ref (join (pull x) (pull y)) (pull (join x y)).
     Proof.
       eapply join_lub; rstep; auto using join_ub_l, join_ub_r.
     Qed.
 
-    Lemma pull_top w :
-      pull w top = top.
+    Lemma pull_top :
+      pull top = top.
     Proof.
       apply antisymmetry; auto using top_ref.
       intros t _. simpl.
@@ -1106,34 +1101,33 @@ Module Behavior.
         + subst x2. destruct H; eauto.
           *)
 
-    Definition sim : klr W (t M1 N1 A) (t M2 N2 B) :=
-      fun w x y => ref x (pull w y).
+    Definition sim : rel (t M1 N1 A) (t M2 N2 B) :=
+      fun x y => ref x (pull y).
 
-    Lemma pull_ret w b :
-      pull w (ret b) = choose (fun a => R w a b).
+    Lemma pull_ret b :
+      pull (ret b) = choose (fun a => R a b).
     Proof.
     Admitted.
 
   End SIM.
 
   Global Instance pull_ref_params :
-    Params (@pull) 2.
+    Params (@pull) 1.
 
   Instance kf_unit : KripkeFrame unit unit :=
     { acc l w w' := True }.
 
   Lemma pull_eq {M N A} (x : t M N A) :
-    pull (k eq) (k eq) (k eq) tt x = x.
+    pull (W:=unit) (k eq) (k eq) eq x = x.
   Proof.
     apply antisymmetry.
     - intros t Ht. cbn in Ht.
-      induction Ht; unfold k in *; subst; eauto using closed.
-      + specialize (H3 tt). cbn in *. eauto.
+      induction Ht; unfold k in *; subst; cbn in *; eauto using closed.
     - intros t. revert x.
       induction t; intros; unfold k in *; try solve [ econstructor; eauto ].
       + apply pull_move with tt m; cbn; auto.
       + apply pull_tcons with tt m; cbn; eauto using closed.
-        intros [ ] ? _ ?; subst.
+        intros; subst.
         apply IHt. cbn. auto.
   Qed.
 
@@ -1141,19 +1135,20 @@ Module Behavior.
     Hint Resolve pull_has_undef_inv.
 
   Lemma sim_r M N {A B} (R : rel A B) :
-    eqrel (sim (k eq) (k eq) (k R) tt) (r M N R).
+    eqrel (sim (W:=unit) (k eq) (k eq) R) (r M N R).
   Proof.
     split.
     - intros x y Hxy t Ht.
       apply Hxy in Ht; clear x Hxy.
       induction Ht; unfold k in *; subst; eauto.
-      cbn in H3. edestruct H3 as (s & Hs & Hst); eauto.
+      cbn in H2. edestruct H2 as (s & Hs & Hst); eauto.
     - intros x y Hxy t Ht.
       apply Hxy in Ht as (s & Hs & Hst); clear x Hxy. unfold k; cbn.
       revert y Hs. assert (tt ~> tt) by constructor.
       induction Hst; intros; eauto.
-      + eapply pull_tcons; eauto using closed.
-        intros [ ] ? _ ?; subst. eauto.
+      + eapply pull_move; eauto. constructor.
+      + eapply pull_tcons; eauto using closed. constructor.
+        intros; subst. eauto.
   Qed.
 
   Section SIM_PROP.
@@ -1162,9 +1157,9 @@ Module Behavior.
     Global Instance ret_sim :
       Monotonic
         (@ret)
-        (forallr RM, forallr RN, forallr R, |= R ++> sim RM RN R).
+        (forallr RM, forallr RN, forallr R, R ++> sim (W:=W) RM RN R).
     Proof.
-      intros M1 M2 RM N1 N2 RN A B R w a b Hab _ [ ].
+      intros M1 M2 RM N1 N2 RN A B R a b Hab _ [ ].
       eapply pull_val; cbn; eauto.
     Qed.
 
@@ -1172,14 +1167,14 @@ Module Behavior.
       Monotonic
         (@bind)
         (forallr RM, forallr RN, forallr RA, forallr RB,
-           (|= RA ++> sim RM RN RB) ++> (|= sim RM RN RA ++> sim RM RN RB)).
+           (RA ++> sim (W:=W) RM RN RB) ++> sim RM RN RA ++> sim RM RN RB).
     Proof.
-      intros M1 M2 RM N1 N2 RN A1 A2 RA B1 B2 RB f1 f2 Hf w x1 x2 Hx.
-      intros t1 (s1 & Hs1 & Hst1). revert w x1 x2 Hx Hs1.
+      intros M1 M2 RM N1 N2 RN A1 A2 RA B1 B2 RB f1 f2 Hf x1 x2 Hx.
+      intros t1 (s1 & Hs1 & Hst1). revert x1 x2 Hx Hs1.
       induction Hst1; intros; eauto.
       - apply Hx in Hs1. inversion Hs1; clear Hs1; subst.
-        + assert (ref (pull RM RN RB w (f2 b)) (pull RM RN RB w (x2 >>= f2))).
-          transitivity (pull RM RN RB w (ret b >>= f2)).
+        + assert (ref (pull RM RN RB (f2 b)) (pull RM RN RB (x2 >>= f2))).
+          transitivity (pull RM RN RB (ret b >>= f2)).
           * rewrite ret_bind. reflexivity.
           * apply has_ret in H1. repeat rstep.
             subst. reflexivity.
@@ -1198,44 +1193,44 @@ Module Behavior.
         + repeat (econstructor; eauto).
         + eapply pull_tcons; eauto. econstructor; eauto.
           intros.
-          change (has (pull RM RN RB w'' (delta (x2 >>= f2) m2 n2)) t0).
-          cut (has (pull RM RN RB w'' (delta x2 m2 n2 >>= f2)) t0); eauto.
+          change (has (pull RM RN RB (delta (x2 >>= f2) m2 n2)) t0).
+          cut (has (pull RM RN RB (delta x2 m2 n2 >>= f2)) t0); eauto.
           repeat rstep. apply delta_bind.
           eapply IHHst1; eauto.
-          2: instantiate (1 := pull RM RN RA w'' (delta x2 m2 n2)); cbn; eauto.
+          2: instantiate (1 := pull RM RN RA (delta x2 m2 n2)); cbn; eauto.
           red. reflexivity.
     Qed.
 
     Global Instance interact_sim :
       Monotonic
         (@interact)
-        (forallr RM, forallr RN, |= (<> RM) ++> sim RM RN RN).
+        (forallr RM, forallr RN, |= RM ++> k1 (sim (W:=W) RM RN) RN).
     Proof.
-      intros M1 M2 RM N1 N2 RN w m1 m2 (w' & Hw' & Hm).
+      intros M1 M2 RM N1 N2 RN w m1 m2 Hm.
       intros t [Ht | [n1 Ht]]; subst; cbn.
       - eapply pull_move; cbn; eauto.
       - eapply pull_tcons; cbn; eauto.
-        intros w'' n2 Hw'' Hn.
+        intros n2 Hn.
         eapply pull_val; cbn; eauto.
     Qed.
 
     Global Instance join_sim :
       Monotonic
         (@join)
-        (forallr RM, forallr RN, forallr RX,
-         |= sim RM RN RX ++> sim RM RN RX ++> sim RM RN RX).
+        (forallr RM : klr W, forallr RN : klr W, forallr RX : rel,
+         sim RM RN RX ++> sim RM RN RX ++> sim RM RN RX).
     Proof.
       intros M1 M2 RM N1 N2 RN X1 X2 RX.
-      intros w x1 x2 Hx y1 y2 Hy. unfold sim in *.
+      intros x1 x2 Hx y1 y2 Hy. unfold sim in *.
       rewrite Hx, Hy. apply join_pull.
     Qed.
 
     Global Instance guard_sim :
       Monotonic
         (@guard)
-        (forallr RM, forallr RN, |= k impl ++> sim RM RN (k ⊤)).
+        (forallr RM : klr W, forallr RN : klr W, impl ++> sim RM RN ⊤).
     Proof.
-      intros M1 M2 RM N1 N2 RN w P Q HPQ. unfold sim in *.
+      intros M1 M2 RM N1 N2 RN P Q HPQ. unfold sim in *.
       intros t (H & Ht); subst.
       eapply pull_val. simpl. eauto. constructor.
     Qed.
@@ -1243,10 +1238,10 @@ Module Behavior.
     Global Instance choose_sim :
       Monotonic
         (@choose)
-        (forallr RM, forallr RN, forallr RA,
-         |= (k1 set_le RA) ++> sim RM RN RA).
+        (forallr RM : klr W, forallr RN : klr W, forallr RA,
+         set_le RA ++> sim RM RN RA).
     Proof.
-      intros M1 M2 RM N1 N2 RN A1 A2 RA w P Q HPQ. unfold sim in *.
+      intros M1 M2 RM N1 N2 RN A1 A2 RA P Q HPQ. unfold sim in *.
       intros t (a & Ha & Ht); subst.
       edestruct HPQ as (b & Hb & Hab); eauto.
       apply pull_val with b; eauto.
@@ -1255,12 +1250,12 @@ Module Behavior.
 
     Context {M1 M2} (RM : klr W M1 M2).
     Context {N1 N2} (RN : klr W N1 N2).
-    Context {X1 X2} (RX : klr W X1 X2).
+    Context {X1 X2} (RX : rel X1 X2).
 
     Global Instance sim_ref :
       Monotonic
-        (@sim _ _ M1 M2 RM N1 N2 RN X1 X2 RX)
-        (- ==> ref --> ref ++> impl).
+        (@sim _ M1 M2 RM N1 N2 RN X1 X2 RX)
+        (ref --> ref ++> impl).
     Proof.
       unfold sim. repeat rstep. intro.
       etransitivity; eauto.
@@ -1269,12 +1264,12 @@ Module Behavior.
     Qed.
 
     Global Instance sim_ref_params :
-      Params (@sim) 3.
+      Params (@sim) 2.
 
-    Lemma join_lub_sim w x y z :
-      sim RM RN RX w x z ->
-      sim RM RN RX w y z ->
-      sim RM RN RX w (join x y) z.
+    Lemma join_lub_sim x y z :
+      sim RM RN RX x z ->
+      sim RM RN RX y z ->
+      sim RM RN RX (join x y) z.
     Proof.
       apply join_lub.
     Qed.
