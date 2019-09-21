3c8ff01c795dd0c92de6edd5867436c1e8a409bc
Yuting Wang <yuting.wang@yale.edu>
2019-09-21 17:30:16 -0400

    Fixed an error

--- driver/Compiler.v
+++ driver/Compiler.v
@@ -208,7 +208,7 @@ Definition transf_c_program_reloc (p: Csyntax.program) : res RelocProgram.progra
   transf_c_program_real p
   @@@ time "Make local jumps use offsets instead of labels" Asmlabelgen.transf_program
   @@ time "Pad Nops to make the alignment of functions correct" PadNops.transf_program
-  @@ time "Pad space to make the alignment of data correct" PadNops.transf_program
+  @@ time "Pad space to make the alignment of data correct" PadInitData.transf_program
   @@@ time "Generation of the symbol table" Symbtablegen.transf_program
   @@@ time "Normalize the symbol table indexes" NormalizeSymb.transf_program
   @@@ time "Generation of relocation table" RelocAsmgen.transf_program
