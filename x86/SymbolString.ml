2c784aa7de0625b43381367026796792fc659896
Yuting Wang <yuting.wang@yale.edu>
2019-09-23 15:31:49 -0400

    Updated the symbol generation file

--- x86/SymbolString.ml
+++ x86/SymbolString.ml
@@ -10,7 +10,29 @@ open Camlcoq
 open Errors
 open Integers
 open BinNums
-open extraction.ExtrOcamlIntConv
+
+let rec int_to_pos (i:int) : positive =
+  if i = 1 then
+    Coq_xH
+  else if i > 1 then      
+    let un = int_to_pos (i / 2) in
+    let rm = i mod 2 in
+    if i = 0 then
+      Coq_xO un
+    else
+      Coq_xI un
+  else
+    let s = sprintf "%i" i in
+    raise (Invalid_argument s)
+
+let rec int_to_Z (i:int) : coq_Z =
+  if i = 0 then
+    Z0
+  else if i > 0 then
+    Zpos (int_to_pos i)
+  else
+    Zneg (int_to_pos i)
+
 
 let string_to_list (s:string) : char list =
   let l = ref [] in
@@ -20,7 +42,7 @@ let string_to_list (s:string) : char list =
 let find_symbol_string_bytes a : Byte.int res =
   try 
     let s = (Hashtbl.find string_of_atom a) in
-    let coq_zs = List.map (fun c -> z_of_int (Char.code c)) (string_to_list s) in
+    let coq_zs = List.map (fun c -> int_to_Z (Char.code c)) (string_to_list s) in
     OK (List.map (fun z -> Byte.repr z) coq_zs)
   with
   | Not_found ->
