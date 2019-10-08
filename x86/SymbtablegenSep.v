909d0c758c23aea15df63566cbfc5bc2fcab28ef
Yuting Wang <yuting.wang@yale.edu>
2019-10-08 11:27:23 -0400

    Progress on the separate compilation for Symbtablegen

--- x86/SymbtablegenSep.v
+++ x86/SymbtablegenSep.v
@@ -111,26 +111,51 @@ Qed.
 (*   unfold link_symbtable. *)
 
 
-(* Lemma link_gen_symb_comm : forall defs1 defs2 defs stbl1 stbl2 dsz1 csz1 dsz2 csz2, *)
-(*     link_defs is_fundef_internal defs1 defs2 = Some defs -> *)
-(*     gen_symb_table sec_data_id sec_code_id defs1 = (stbl1, dsz1, csz1) -> *)
-(*     gen_symb_table sec_data_id sec_code_id defs2 = (stbl2, dsz2, csz2) -> *)
-(*     exists stbl, *)
-(*       link_symbtable (reloc_offset_fun (create_data_section defs1) (create_code_section defs1)) *)
-(*                      stbl1 stbl2 = Some stbl *)
-(*       /\ gen_symb_table sec_data_id sec_code_id defs = (stbl, dsz1 + dsz2, csz1 + csz2). *)
-(* Proof. *)
+
+Definition symbentry_index_in_range range e :=
+  match symbentry_secindex e with
+  | secindex_normal i => In i range
+  | _ => True
+  end.
+
+Definition symbtable_indexes_in_range range t :=
+  Forall (symbentry_index_in_range range) t.
+
+Lemma gen_symb_table_index_in_range : forall defs sec_data_id sec_code_id stbl dsz csz,
+    gen_symb_table sec_data_id sec_code_id defs = (stbl, dsz, csz) ->
+    symbtable_indexes_in_range (map SecIndex.interp [sec_data_id; sec_code_id]) stbl.
+Admitted.
+
+Lemma reloc_symbtable_exists : forall stbl f dsz csz,
+    symbtable_indexes_in_range (map SecIndex.interp [sec_data_id; sec_code_id]) stbl ->
+    f = (reloc_offset_fun dsz csz) ->
+    exists stbl', reloc_symbtable f stbl = Some stbl' /\
+             Forall2 (fun e1 e2 => reloc_symb f e1 = Some e2) stbl stbl'.
+Admitted.
+
+
+Lemma link_gen_symb_comm : forall defs1 defs2 defs stbl1 stbl2 dsz1 csz1 dsz2 csz2 f_ofs,
+    link_defs is_fundef_internal defs1 defs2 = Some defs ->
+    gen_symb_table sec_data_id sec_code_id defs1 = (stbl1, dsz1, csz1) ->
+    gen_symb_table sec_data_id sec_code_id defs2 = (stbl2, dsz2, csz2) ->
+    f_ofs = reloc_offset_fun dsz1 csz1 ->
+    exists stbl stbl2',
+      reloc_symbtable f_ofs stbl2 = Some stbl2' /\
+      link_symbtable stbl1 stbl2' = Some stbl
+      /\ gen_symb_table sec_data_id sec_code_id defs = (stbl, dsz1 + dsz2, csz1 + csz2).
+Proof.
+Admitted.
 (*   intros defs1 defs2 defs stbl1 stbl2 dsz1 csz1 dsz2 csz2 LINK GS1 GS2. *)
 (*   unfold link_defs in LINK. *)
 (*   unfold gen_symb_table in GS1, GS2. *)
-(*   destruct (fold_left (acc_symb sec_data_id sec_code_id) defs1 ([dummy_symbentry], 0, 0))  *)
+(*   destruct (fold_left (acc_symb sec_data_id sec_code_id) defs1 ([dummy_symbentry], 0, 0)) *)
 (*     as (r1 & csz1') eqn:GSEQ1. destruct r1 as (rstbl1 & dsz1'). inv GS1. *)
-(*   destruct (fold_left (acc_symb sec_data_id sec_code_id) defs2 ([dummy_symbentry], 0, 0))  *)
+(*   destruct (fold_left (acc_symb sec_data_id sec_code_id) defs2 ([dummy_symbentry], 0, 0)) *)
 (*     as (r2 & csz') eqn:GSEQ2. destruct r2 as (rstbl2 & dsz2'). inv GS2. *)
 (*   unfold gen_symb_table. *)
 (*   exploit link_defs_acc_symb_comm; eauto. *)
 (*   destruct 1 as (stbl & LINKS & ACC). *)
-(*   exists stbl. split; auto. rewrite ACC.  *)
+(*   exists stbl. split; auto. rewrite ACC. *)
 (*   rewrite rev_involutive. auto. *)
 (* Qed. *)
 
@@ -205,7 +230,6 @@ Proof.
   setoid_rewrite LINK.
   apply link_prog_inv in LINK.
   destruct LINK as (MAINEQ & NRPT1 & NRPT2 & defs & PEQ & LINKDEFS). subst. simpl.
-  unfold link_defs in LINKDEFS.
   unfold match_prog in *.
 
   unfold transf_program in MATCH1.
@@ -219,30 +243,53 @@ Proof.
   destruct (gen_symb_table sec_data_id sec_code_id (AST.prog_defs p2)) as (p & csz2) eqn:GSEQ2 .
   destruct p as (stbl2 & dsz2).
   destruct zle; try monadInv MATCH2; simpl.
-
-  (* unfold link_symbtable. *)
-  (* exploit link_gen_symb_comm; eauto. *)
-  (* destruct 1 as (stbl & LINKS & GENS). *)
-  (* eexists. split. *)
-  (* setoid_rewrite LINKS. reflexivity. *)
-  (* unfold transf_program. *)
-
-  (* exploit link_pres_wf_prog; eauto. *)
-  (* intros WF.  *)
-  (* destruct check_wellformedness; try congruence. *)
-  (* simpl. rewrite GENS. *)
   
-  (* destruct zle.  *)
-  (* repeat f_equal. *)
-  (* unfold create_sec_table. repeat f_equal. *)
-  (* unfold create_data_section. f_equal. *)
-  (* rewrite fold_right_acc_init_data_app. *)
-  (* rewrite <- fold_right_app. *)
-  (* apply link_acc_init_data_comm; auto. *)
-  (* unfold create_code_section. f_equal. *)
-  (* rewrite fold_right_acc_instrs_app. *)
-  (* rewrite <- fold_right_app. *)
-  (* apply link_acc_instrs_comm; auto. *)
+
+  (* generalize (gen_symb_table_index_in_range _ _ _ GSEQ2). *)
+  (* intro SIDX_RANGE2.   *)
+  (* generalize (reloc_symbtable_exists  *)
+  (*               SIDX_RANGE2  *)
+  (*               (eq_refl  *)
+  (*                  (reloc_offset_fun (sec_size (create_data_section (AST.prog_defs p1))) *)
+  (*                                    (sec_size (create_code_section (AST.prog_defs p1)))))). *)
+  (* destruct 1 as (stbl2' & RELOC2 & RELOC_REL). *)
+  (* setoid_rewrite RELOC2. *)
+
+  exploit link_gen_symb_comm; eauto.
+  destruct 1 as (stbl & stbl2' & RELOC & LINKS & GENS).
+
+  Lemma gen_symb_table_size: forall defs d_id c_id stbl dsz csz,
+      gen_symb_table d_id c_id defs = (stbl, dsz, csz) ->
+      sec_size (create_data_section defs) = dsz /\
+      sec_size (create_code_section defs) = csz.
+  Admitted.
+
+  generalize (gen_symb_table_size _ _ _ GSEQ1).
+  destruct 1 as (DSZ & CSZ).
+  setoid_rewrite DSZ.
+  setoid_rewrite CSZ.
+  rewrite RELOC.
+  rewrite LINKS.
+
+  eexists. split. reflexivity.
+  unfold transf_program.
+
+  exploit link_pres_wf_prog; eauto.
+  intros WF.
+  destruct check_wellformedness; try congruence.
+  simpl. rewrite GENS.
+  
+  destruct zle.
+  repeat f_equal.
+  unfold create_sec_table. repeat f_equal.
+  unfold create_data_section. f_equal.
+  rewrite fold_right_acc_init_data_app.
+  rewrite <- fold_right_app.
+  apply link_acc_init_data_comm; auto.
+  unfold create_code_section. f_equal.
+  rewrite fold_right_acc_instrs_app.
+  rewrite <- fold_right_app.
+  apply link_acc_instrs_comm; auto.
 
   Admitted.
 
