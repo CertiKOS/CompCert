2a2ecdc2e9d1cd0950f69fde0ee9e2e1da8cd220
Yuting Wang <yuting.wang@yale.edu>
2019-10-30 17:34:07 -0400

    Admitted the lemma that the semantics of RealAsm is preserved under permutation of definitions

---
+++ x86/PermuteRealAsmproof.v
@@ -0,0 +1,38 @@
+(* *******************  *)
+(* Author: Yuting Wang  *)
+(* Date:   Oct 30, 2019 *)
+(* *******************  *)
+
+(** * Preservation of semantics under the permutation of definitions for RealAsm *)
+Require Import Coqlib Errors.
+Require Import Integers Floats AST Linking.
+Require Import Values Memory Events Globalenvs Smallstep.
+Require Import Permutation.
+Require Import Asm RealAsm.
+
+Definition match_prog (p: Asm.program) (tp: Asm.program) :=
+  Permutation (prog_defs p) (prog_defs tp).
+
+Lemma transf_program_match:
+  forall p, match_prog p p.
+Proof.
+  intros. red. apply Permutation_refl.
+Qed.
+
+
+(** Preservation of semantics under permutation *)
+Section PRESERVATION.
+
+Context `{external_calls_prf: ExternalCalls}.
+
+Variable prog: Asm.program.
+Variable tprog: Asm.program.
+Hypothesis TRANSF: match_prog prog tprog.
+
+Let ge := Genv.globalenv prog.
+Let tge := Genv.globalenv tprog.
+
+Theorem transf_program_correct:
+  forward_simulation (RealAsm.semantics prog (Pregmap.init Vundef))
+                     (RealAsm.semantics tprog (Pregmap.init Vundef)).
+Admitted.
